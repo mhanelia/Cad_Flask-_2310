bbd1428146f1c558896c916549402a366cff3ccf
mhanelia <murilo.aippe@hotmail.com>
2017-10-23 16:25:48 -0200

    update

--- routes.py
+++ routes.py
@@ -14,7 +14,17 @@ def home():
 def about():
 
     return render_template('about.html')
-    
+
+@app.route('/cadastrar')
+def cadastrar():
+
+    return render_template('cadastrar.html')
+
+@app.route('/lista')
+def lista():
+    parent = [{'Nome':'Murilo','Idade':'15','Email':'murilo.aippe@hotmail.com'}]
+    return render_template('lista.html', parent_dict=parent)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
